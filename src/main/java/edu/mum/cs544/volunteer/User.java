9cce2e8c7a8826cb8cfb00bc2310bf8fcd8ed416
Ivan <ivanatarah@yahoo.com>
2016-08-14 18:54:54 -0500

    initial commit

---
+++ src/main/java/edu/mum/cs544/volunteer/User.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.mum.cs544.volunteer;
+
+/**
+ * @author Ivan
+ *
+ */
+public abstract class User {
+	private int id;
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private UserRole role;
+}
---
+++ src/main/java/edu/mum/cs544/volunteer/UserRole.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.mum.cs544.volunteer;
+
+/**
+ * @author Ivan
+ *
+ */
+public enum UserRole {
+	ADMINISTRATOR, VOLUNTEER
+}
